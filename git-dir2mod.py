fb7d11d1534c2b164a8c33ab4f63acc16d8954da
Thomas Gläßle <t_glaessle@gmx.de>
2017-06-11 03:09:48 +0200

    Add command line arguments + module descriptions

--- git-dir2mod.py
+++ git-dir2mod.py
@@ -1,35 +1,66 @@
 #!/usr/bin/env python
+"""
+History rewrite helper script: Convert subfolder to submodule.
+
+Usage:
+    ./git-dir2mod.py TREEMAP FOLDER URL [NAME]
+
+Arguments:
+
+    TREEMAP     Path to tree index. For every top-level tree there should be
+                a file $TREEMAP/$TREE_SHA1 that contains the SHA1 of the
+                target commit.
+
+    FOLDER      Subfolder to replace.
+
+    URL         URL of the submodule
+
+    NAME        Name of the submodule (defaults to FOLDER)
+
+This will leave you with with an `objmap` folder in the current directory
+that maps top level trees to other the rewritten trees, i.e.
+
+    echo NEW_SHA1 > objmap/OLD_SHA1
+"""
+
 from tree_filter import TreeFilter, cached, write_blob
+
+import os
+import sys
 
 
 class Dir2Mod(TreeFilter):
 
-    NAME = 'examples'
-    PATH = 'examples'
-    URL = 'https://github.com/MethodicalAcceleratorDesign/madx-examples.git'
+    def __init__(self, treemap, folder, url, name=None):
+        self.treemap = treemap
+        self.folder = folder
+        self.url = url
+        self.name = name or folder
 
+    # TODO: nested path
     def rewrite_object(self, mode, kind, sha1, name):
-        if kind == 'tree' and name == 'examples':
+        if kind == 'tree' and name == self.folder:
             return self.rewrite_tree(mode, kind, sha1, name)
         return [(mode, kind, sha1, name)]
 
     @cached
     def rewrite_tree(self, mode, kind, sha1, name):
-        commit = open('../treemap/'+sha1).read().strip()
+        commit = open(os.path.join(self.treemap, sha1)).read().strip()
         return [
             ('160000', 'commit', commit, name),
             self.gitmodules_file(),
         ]
 
+    # TODO: append to existing
     @cached
     def gitmodules_file(self):
         sha1 = write_blob("""
 [submodule "{}"]
 	path = {}
 	url = {}
-"""[1:].format(self.NAME, self.PATH, self.URL))
+"""[1:].format(self.name, self.folder, self.url))
         return ('100644', 'blob', sha1, '.gitmodules')
 
 
 if __name__ == '__main__':
-    Dir2Mod().main()
+    Dir2Mod(*sys.argv[1:]).main()
--- git-unpack.py
+++ git-unpack.py
@@ -1,20 +1,46 @@
 #!/usr/bin/env python
+"""
+History rewrite helper script: Unzip files in history
+
+Usage:
+    ./git-unpack.py [EXT] [PROG]
+
+Arguments:
+
+    EXT         Filename extension          [default: .gz]
+    PROG        Program to run for the file [default: gunzip]
+
+Example:
+
+    ./git-unpack.py .gz gunzip
+
+This will leave you with with an `objmap` folder in the current directory
+that maps top level trees to other the rewritten trees, i.e.
+
+    echo NEW_SHA1 > objmap/OLD_SHA1
+"""
+
 from tree_filter import TreeFilter, cached
 
 import os
+import sys
 
 
-class UnpackGz(TreeFilter):
+class Unpack(TreeFilter):
+
+    def __init__(self, ext='.gz', unz='gunzip'):
+        self.ext = ext
+        self.unz = unz
 
     @cached
     def rewrite_file(self, mode, kind, sha1, name):
-        if not name.endswith('.ref.gz'):
+        if not name.endswith(self.ext):
             return [(mode, kind, sha1, name)]
         base, ext = os.path.splitext(name)
-        cmd = "git cat-file blob {} | gunzip | git hash-object -w -t blob --stdin"
-        sha1 = os.popen(cmd.format(sha1)).read().strip()
+        cmd = "git cat-file blob {} | {} | git hash-object -w -t blob --stdin"
+        sha1 = os.popen(cmd.format(sha1, self.unz)).read().strip()
         return [(mode, kind, sha1, base)]
 
 
 if __name__ == '__main__':
-    UnpackGz().main()
+    Unpack(*sys.argv[1:]).main()
