816fae8c2b13b747b86764017fedc74f3f0b2f76
Thomas Gläßle <t_glaessle@gmx.de>
2017-07-08 22:20:03 +0200

    Simplify ETA
    The new version is based on the overall average rate rather than an
    attempt to guess the current rate. This may give worse predictions for
    ETA, but it feels smoother and is a lot less ugly.

--- git_filter_tree/tree_filter.py
+++ git_filter_tree/tree_filter.py
@@ -170,31 +170,19 @@ class TreeFilter(object):
 
         pending = len(trees)
         done = 0
-        tstart = time.time()
-        checkpoint_done = 0
-        checkpoint_time = tstart
+        start = time.time()
 
         for _ in pool.imap_unordered(self.rewrite_root, trees):
         #for _ in map(self.rewrite_root, trees):
             done += 1
-            now = time.time()
-            done_since_checkpoint = done - checkpoint_done
-            compl_rate = (now - checkpoint_time) / done_since_checkpoint
-            eta = time_to_str((pending - done) * compl_rate)
+            rate = (time.time() - start) / done
+            eta = time_to_str((pending - done) * rate)
             print('\r{} / {} Trees rewritten ({:.1f} trees/sec), ETA: {}          '
-                  .format(done, pending, 1 / compl_rate, eta), end='')
+                  .format(done, pending, 1 / rate, eta), end='')
             sys.stdout.flush()
-            # Keep a window of the last 5s of rewrites for ETA calculation.
-            if now - checkpoint_time > 5:
-                checkpoint_done = done
-                checkpoint_time = now
 
         pool.close()
         pool.join()
-
-        elapsed = time.time() - tstart
-        print('\nTree rewrite completed in {} ({:.1f} trees/sec)'
-              .format(time_to_str(elapsed), done / elapsed))
 
         return 0
 
